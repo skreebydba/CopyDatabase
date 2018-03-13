ae2672b25aba7ef74e04131ebe9250d8de47bc79
skreebydba <skreebydba@users.noreply.github.com>
2018-03-13 10:41:06 -0500

    Update RunOla.ps1

--- RunOla.ps1
+++ RunOla.ps1
@@ -5,17 +5,18 @@ Login-AzureRmAccount;
 $cred = Get-Credential;
 [string]$user = $cred.UserName;
 $pass = $cred.GetNetworkCredential().Password;
+$sqlserver = "yoursqlserver.database.windows.net";
 $dbccquery = "EXECUTE [dbo].[DatabaseIntegrityCheck] @Databases = 'USER_DATABASES', @LogToTable = 'Y';"
 $indexquery = "EXECUTE [dbo].[IndexOptimize] @Databases = 'USER_DATABASES', @LogToTable = 'Y';"
 
-$databases = Invoke-Sqlcmd -ServerInstance "fbgguggenheimsrv.database.windows.net" -Database master -Username $user -Password $pass -Query "SELECT name FROM sys.databases WHERE database_id > 4";
+$databases = Invoke-Sqlcmd -ServerInstance $sqlserver -Database master -Username $user -Password $pass -Query "SELECT name FROM sys.databases WHERE database_id > 4";
 
 $databases;
 
 foreach($database in $databases)
 {
 
-Invoke-Sqlcmd -ServerInstance "fbgguggenheimsrv.database.windows.net" -Database $database.name -Username $user -Password $pass -Query $dbccquery;
-Invoke-Sqlcmd -ServerInstance "fbgguggenheimsrv.database.windows.net" -Database $database.name -Username $user -Password $pass -Query $indexquery;
+Invoke-Sqlcmd -ServerInstance $sqlserver -Database $database.name -Username $user -Password $pass -Query $dbccquery;
+Invoke-Sqlcmd -ServerInstance $sqlserver -Database $database.name -Username $user -Password $pass -Query $indexquery;
 
 }
